026736c2209a2876f9c6c8537dda2b3362251f6d
Shawna Foo <sfoo@asu.edu>
2019-05-22 13:31:23 -1000

    Reorganising

--- Analysis.R
+++ Analysis.R
@@ -1,2 +1,6 @@
 ##Import patch reef measure data sheet
 ##Can import by just importing the data directly through the files
+
+##Merge files from every person
+
+merged <- rbind(Module1_MeasuringReefs_SF, ASL_data)
