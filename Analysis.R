5560215958fc7bb62f945ec0ac63fcfa893c0643
Shawna Foo <sfoo@asu.edu>
2019-05-22 14:16:41 -1000

    Updating files

--- Analysis.R
+++ Analysis.R
@@ -7,6 +7,19 @@ merged <- rbind(Module1_MeasuringReefs_SF, ASL_data, Module1_MeasuringReefs_AC,
                 Module1_MeasuringReefs_NSH, Module1_MeasuringReefs_TJQ,
                 Module1_MeasuringReefs_Template)
 
+##need to remove field data for patch reef 12
+#method = field
+#reef_no = 12
+
+merged <- merged[!(merged$reef_no == "12" & merged$method == "field"),]
+
+
+
+d<-d[!(d$A=="B" & d$E==0),]
+
+
 library(xlsx)
 
 groupdata <- write.xlsx(merged, "groupdata.xlsx")
+groupdata <- write.csv(merged, "groupdata.csv")
+
